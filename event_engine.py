577ef0592b7b741c7aacb382c039861245e245fb
rodmagaldi <rod.magaldi@gmail.com>
2021-05-19 22:09:48 -0300

    Pick letters from symbols without regex

--- event_engine.py
+++ event_engine.py
@@ -1,16 +1,12 @@
-import re
-from string import punctuation
 from functools import cmp_to_key
 import locale
 
-from const import BEGIN_CAPITAL_LETTERS, END_CAPITAL_LETTERS, BEGIN_CAPITAL_SPECIAL_LETTERS, END_CAPITAL_SPECIAL_LETTERS
+from const import BEGIN_CAPITAL_LETTERS, CR, END_CAPITAL_LETTERS, BEGIN_CAPITAL_SPECIAL_LETTERS, END_CAPITAL_SPECIAL_LETTERS, BEGIN_LOWER_CASE_LETTERS, END_LOWER_CASE_LETTERS, BEGIN_LOWER_CASE_SPECIAL_LETTERS, END_LOWER_CASE_SPECIAL_LETTERS, BEGIN_NUMBERS, END_NUMBERS, LF, SPACE
 
 locale.setlocale(locale.LC_ALL, 'pt_BR')
 
 
 class EventEngine:
-    regex_compiler_letters = re.compile('[A-Za-z0-9À-ÿ]')
-    regex_compiler_blank = re.compile('[\n\r\s]')
 
     def __init__(self, input_text, keywords):
         self.input_text = input_text
@@ -93,21 +89,16 @@ class EventEngine:
         return self.ordered_count
 
     def is_letter_or_number(self, character):
-        match = EventEngine.regex_compiler_letters.match(character)
-        if match:
-            return match.group()
-        else:
-            return None
+        byte_list = list(character.encode('utf-8'))
+        new_char = byte_list[-1]
+        return (BEGIN_NUMBERS <= new_char <= END_NUMBERS) or (BEGIN_CAPITAL_LETTERS <= new_char <= END_CAPITAL_LETTERS) or (BEGIN_LOWER_CASE_LETTERS <= new_char <= END_LOWER_CASE_LETTERS) or (BEGIN_LOWER_CASE_SPECIAL_LETTERS <= new_char <= END_LOWER_CASE_SPECIAL_LETTERS) or (BEGIN_CAPITAL_SPECIAL_LETTERS <= new_char <= END_CAPITAL_SPECIAL_LETTERS)
 
     def is_space_or_linebreak(self, character):
-        match = EventEngine.regex_compiler_blank.match(character)
-        if match:
-            return match.group()
-        else:
-            return None
+        byte_list = list(character.encode('utf-8'))
+        return byte_list[-1] in [CR, LF, SPACE]
 
     def is_punctuation(self, character):
-        return character in punctuation
+        return (not self.is_letter_or_number(character) and not self.is_space_or_linebreak(character))
 
     def is_current_word_valid(self):
         return self.current_word != ""
--- runner.py
+++ runner.py
@@ -1,23 +1,24 @@
 from event_engine import EventEngine
 
+
 class Runner:
     def __init__(self, input_file_path, keywords):
         self.input_file_path = input_file_path
         self.keywords = keywords
         self.file_content = ""
-    
+
     def configure_file(self):
         with open(self.input_file_path, 'rb') as text_file:
             self.file_content = text_file.read()
-        
+
     def configure_keywords(self):
         self.keywords = list(map(lambda item: item.lower(), self.keywords))
-        
+
     def run(self):
         self.configure_file()
         self.configure_keywords()
-        
+
         word_counter = EventEngine(self.file_content, self.keywords)
         word_counter.run()
-        
+
         return word_counter.generate_results()
